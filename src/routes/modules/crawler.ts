fdae3b559e526bea487b6e529f3fe57ef1d13862
yongsin0129 <yongsin0129@gmail.com>
2022-11-30 18:10:58 +0800

    refactor : 修改 (新增網站最新電影資料進 database 的邏輯)

--- src/routes/modules/crawler.ts
+++ src/routes/modules/crawler.ts
@@ -23,9 +23,7 @@ async function main () {
   const onlineMovieList = await getOnlineMovieList(URL_FirstRound)
 
   // 將網站上最新的首輪電影清單加入到資料庫
-  for (let index = 0; index < onlineMovieList.length; index++) {
-    await updateDatabase(onlineMovieList[index])
-  }
+  await addNewMovieToDatabase(onlineMovieList, databaseMovieList)
 
   // 比較 資料庫與網站資料
   await updateFirstRoundMovieList(databaseMovieList, onlineMovieList)
@@ -33,27 +31,28 @@ async function main () {
 
 main()
 
+// 由資料庫中的電影清單比對最新網路清單，並進行更新
 async function updateFirstRoundMovieList (
   databaseMovieList: Prisma.MovieListMaxAggregateOutputType[],
   onlineMovieList: MovieData[]
 ) {
+  // 製做一個最新網路清單的 string 用來比對
   const onlineMovieList_Title_ReleaseDate = JSON.stringify(
     onlineMovieList.map(MovieData => {
       return MovieData.movieTitle + MovieData.movieReleaseDate
     })
   )
 
-  // console.log(onlineMovieList_Title_ReleaseDate)
-
+  // 遍歷資料庫中所有的每一筆資料
   for (let index = 0; index < databaseMovieList.length; index++) {
     const data = databaseMovieList[index]
     if (data.title !== null && data.releaseDate !== null) {
       const title_releaseDate = data.title + data.releaseDate
 
       if (onlineMovieList_Title_ReleaseDate.includes(title_releaseDate)) {
-        // console.log('重複的 : ' + title_releaseDate)
+        // 如果重複，不進行任何動作
       } else {
-        // console.log('沒有重複的 : ' + title_releaseDate)
+        // 如果沒有重複，表示此電影已經不在首輪內
         const needUpdateMovieData = await prisma.movieList.update({
           where: {
             title_releaseDate: {
@@ -65,13 +64,14 @@ async function updateFirstRoundMovieList (
             status: Status.leaveFirstRound
           }
         })
-        console.log('----------本次狀態變換的電影----------')
-        console.log('needUpdateMovieData', needUpdateMovieData)
+        console.log('----------本次狀態更改的電影----------')
+        console.log(needUpdateMovieData)
       }
     }
   }
 }
 
+// 取得資料庫中的電影清單 by status
 async function getDatabaseMovieList (status: Status) {
   const movieList = await prisma.movieList.findMany({
     where: { status }
@@ -79,6 +79,7 @@ async function getDatabaseMovieList (status: Status) {
   return movieList
 }
 
+// 利用爬蟲取得重新的電影清單 by URL
 async function getOnlineMovieList (URL: string) {
   const movieList: MovieData[] = []
 
@@ -97,6 +98,7 @@ async function getOnlineMovieList (URL: string) {
   return movieList
 }
 
+// 格式化日期字串
 function formatReleaseDate (ReleaseDate: string): string {
   const regex = new RegExp(/^[12]\d{3}?\/\d{1,2}?\/\d{1,2}?/)
   let result = ReleaseDate.match(regex)
@@ -104,30 +106,53 @@ function formatReleaseDate (ReleaseDate: string): string {
   else return '1990/01/01' // 無上映時間的統一處理方式
 }
 
-async function updateDatabase (movieData: MovieData) {
-  try {
-    const newMovieData = await prisma.movieList.create({
-      data: {
-        title: movieData.movieTitle,
-        releaseDate: movieData.movieReleaseDate,
-        url: movieData.movieUrl,
-        status: Status.firstRound
-      }
+// 將最新的首輪清單加入至資料庫中
+async function addNewMovieToDatabase (
+  onlineMovieList: MovieData[],
+  databaseMovieList: Prisma.MovieListMaxAggregateOutputType[]
+) {
+  // 製做一個現有資料庫的 string 用來比對
+  const databaseMovieList_Title_ReleaseDate = JSON.stringify(
+    databaseMovieList.map(MovieData => {
+      return MovieData.title! + MovieData.releaseDate!
     })
-    console.log('----------本次更新添加的電影----------')
-    console.log(newMovieData)
-  } catch (error) {
+  )
+
+  for (let index = 0; index < onlineMovieList.length; index++) {
+    const movieData = onlineMovieList[index]
+    const title_releaseDate = movieData.movieTitle + movieData.movieReleaseDate
+    
+    // 先檢查最新的首輪電影資料有無存在 database 之中，若有則跳過
     if (
-      error instanceof Prisma.PrismaClientKnownRequestError &&
-      error?.code === 'P2002'
+      databaseMovieList_Title_ReleaseDate.includes(title_releaseDate) === true
     ) {
-      const errorMsg = {
-        ...error,
-        msg: 'There is a unique constraint violation, a new data cannot be created with this title & releaseDate'
+      continue
+    }
+
+    // 將最新的首輪電影資料存入資料庫之中
+    try {
+      const newMovieData = await prisma.movieList.create({
+        data: {
+          title: movieData.movieTitle,
+          releaseDate: movieData.movieReleaseDate,
+          url: movieData.movieUrl,
+          status: Status.firstRound
+        }
+      })
+      console.log('----------本次更新添加的電影----------')
+      console.log(newMovieData)
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error?.code === 'P2002'
+      ) {
+        const errorMsg = {
+          ...error,
+          msg: 'There is a unique constraint violation, a new data cannot be created with this title & releaseDate'
+        }
+      } else {
+        console.log(error)
       }
-      // console.log(errorMsg)
-    } else {
-      // console.log(error)
     }
   }
 }
