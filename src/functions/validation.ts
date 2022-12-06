464bfb9af7de57e38785afb18fa82dd88914fe77
yongsin0129 <yongsin0129@gmail.com>
2022-12-06 11:57:05 +0800

    feat : add user route signIn

--- src/functions/validation.ts
+++ src/functions/validation.ts
@@ -1,11 +1,11 @@
 import Joi from 'joi'
 
-export interface RegisterInputData {
+export interface LoginInputData {
   email: string
   password: string
 }
 
-export interface LoginInputData {
+export interface RegisterInputData {
   email: string
   password: string
   password_confirmation: string
@@ -15,7 +15,12 @@ export interface LoginInputData {
 export const registerValidation = (registerInputData: RegisterInputData) => {
   const schema = Joi.object({
     email: Joi.string().min(6).max(50).required().email(),
-    password: Joi.string().min(6).max(255).required()
+    password: Joi.string().min(6).max(255).required(),
+    password_confirmation: Joi.any()
+      .equal(Joi.ref('password'))
+      .required()
+      .label('password_confirmation')
+      .messages({ 'any.only': '{{#label}} does not match' })
   })
 
   return schema.validate(registerInputData)
@@ -25,12 +30,7 @@ export const registerValidation = (registerInputData: RegisterInputData) => {
 export const loginValidation = (loginInputData: LoginInputData) => {
   const schema = Joi.object({
     email: Joi.string().min(6).max(50).required().email(),
-    password: Joi.string().min(6).max(255).required(),
-    password_confirmation: Joi.any()
-      .equal(Joi.ref('password'))
-      .required()
-      .label('Confirm password')
-      .messages({ 'any.only': '{{#label}} does not match' })
+    password: Joi.string().min(6).max(255).required()
   })
   return schema.validate(loginInputData)
 }
