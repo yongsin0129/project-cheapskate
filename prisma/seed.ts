a8a118977aee3c3d1fdc01973b65b4b6771b5b67
yongsin0129 <yongsin0129@gmail.com>
2022-12-22 10:11:00 +0800

    feat : add relation between User and MovieList

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,24 +1,31 @@
 import { PrismaClient, Prisma } from '@prisma/client'
 import bcrypt from 'bcryptjs'
-
+import { object } from 'joi'
 const prisma = new PrismaClient()
 
 async function main () {
+  // 先選出現在資料庫中所有電影的 Id
+  const MoiveIdArray: { id: string }[] = await prisma.movieList.findMany({
+    select: { id: true }
+  })
+
+  // 先將 database 的使用者全部清空
   console.log(`users db reset ...`)
   await prisma.user.deleteMany({})
-  console.log(`users db Start seeding ...`)
 
-  for (let index = 1; index <= 10; index++) {
+  // 開始製作假的使用者清單
+  console.log(`users db Start seeding ...`)
+  for (let index = 1; index <= 20; index++) {
     const user = await prisma.user.create({
       data: {
         name: `user${index}`,
         email: `user${index}@example.com`,
-        password: bcrypt.hashSync('123456', 10)
+        password: bcrypt.hashSync('123456', 10),
+        followedMovies: { connect: getArrayRandomElement(MoiveIdArray) }
       }
     })
-    console.log(`Created user with id: ${user.id}`)
+    console.log(`create ${user.name} successfully`)
   }
-
   console.log(`Seeding finished.`)
 }
 
@@ -31,3 +38,21 @@ main()
     await prisma.$disconnect()
     process.exit(1)
   })
+
+/********************************************************************************
+*
+          helper function
+*
+*********************************************************************************/
+function getArrayRandomElement (array: { id: string }[]): { id: string }[] {
+  const randomElementArray: { id: string }[] = []
+  while (randomElementArray.length < 5) {
+    const randomNumber = Math.floor(Math.random() * array.length)
+    const randomElement = array[randomNumber]
+    // 避免選到同一個 movie id
+    if (!randomElementArray.includes(randomElement)) {
+      randomElementArray.push(randomElement)
+    }
+  }
+  return randomElementArray
+}
