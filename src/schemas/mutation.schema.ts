92ad6856c603c9d9acd025e46cecbdd39d2923d8
yongsin0129 <yongsin0129@gmail.com>
2023-04-24 08:10:24 +0800

    doc : 針對 GraphQL query and resolvers 寫註解

--- src/schemas/mutation.schema.ts
+++ src/schemas/mutation.schema.ts
@@ -2,7 +2,10 @@ import gql from 'graphql-tag'
 
 export const Mutation_typeDefs = gql`
   type Mutation {
+    # 使用者將指定電影加入收藏清單
     addFollowedMovies(MovieListId: String): User
+
+    # 使用者將指定電影移除收藏清單
     removeFollowedMovies(MovieListId: String): User
   }
 `
--- src/schemas/query.schema.ts
+++ src/schemas/query.schema.ts
@@ -2,14 +2,21 @@ import gql from 'graphql-tag'
 
 export const query_typeDefs = gql`
   type Query {
+    # 測試 GraphQL server 的 query
     hello: String
+
+    # 取得所有 user 資料 (可根據引數不同篩選資料)
     Users(searchString: String, take: Int, skip: Int): [User!]!
+
+    # 取得所有 movie 資料 (可根據引數不同篩選資料)
     Movies(
       searchString: String
       take: Int
       skip: Int
       status: Status
     ): [MovieList!]!
+
+    # 取得當下使用者資料( header 帶的 token )
     Me: User
   }
 
